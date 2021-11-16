a9a6ca67ec9a8d923f14d0a142b70a64fa57638d
Vipul Jatanna <jatannavips123@gmail.com>
2021-11-17 00:17:08 +0530

    Finally Delete Entry Run

--- MVCProject28/Controllers/ExpenseController.cs
+++ MVCProject28/Controllers/ExpenseController.cs
@@ -37,5 +37,34 @@ namespace MVCProject28.Controllers
             }
             return View(obj);
         }
+        // GET-Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Expenses.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // POST-Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Expenses.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Expenses.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
